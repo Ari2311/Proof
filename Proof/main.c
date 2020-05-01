0c4777090484120e270b1c16d87ad9be55587112
Ari <Ari>
2020-04-30 20:32:24 -0500

    Initial Commit

---
+++ Proof/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proof
+//
+//  Created by Ariadna on 30/04/20.
+//  Copyright © 2020 Ariadna. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
